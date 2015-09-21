4a8fb62f50762feb2a4ba202948da971c2da9ff6
yuko <ike534xx@yahoo.co.jp>
2015-09-21 21:30:16 +0900

    specify the character code

--- find_document.sh
+++ find_document.sh
@@ -6,4 +6,4 @@ date_time=`date '+%Y-%m-%d_%H:%M:%S'`
 find ~/ -atime -1 -print0 | xargs -n 100 -0 stat -f "%d\t%i\t%p\t%l\t%u\t%g\t%r\t%z\t%Sa\t%Sm\t%Sc\t%HT\t%k\t%b\t%f\t%N" -t '%F %T' >> "Documents_file/${date_time}"
 wait
 
-bundle exec ruby create_db.rb "Documents_file/${date_time}"
+bundle exec ruby -Ku create_db.rb "Documents_file/${date_time}"
